2aff2441ac9eadd5e7bcbd5c7ffe46a5ceda4d83
Selim <selimr1989@gmai.com>
2022-02-24 21:52:01 -0500

    Maven project

---
+++ maven-samples/src/main/java/com/example/maven_samples/greeter.java
@@ -0,0 +1,23 @@
+package com.example.maven_samples;
+
+public class greeter {
+
+	/**
+	   * This is a constructor.
+	   */
+	  public greeter() {
+
+	  }
+
+	  
+	  //TODO: Add javadoc comment
+	  public String greet(String someone) {
+	    return String.format("Hello, %s!", someone);
+	  }
+
+	  
+	  public static void main(String args[]){
+	    
+		  System.out.println("Hello from Main Method!!!");
+	  }
+}
